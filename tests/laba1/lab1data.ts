6b67e867dec296237aa8d44a83fd2fd137429159
medmaks <yltramaks731@gmail.com>
2025-06-26 12:13:14 +0300

    t1

---
+++ tests/laba1/lab1data.ts
@@ -0,0 +1,6 @@
+export const formData = {
+  name: 'Максим Слеба',
+  email: 'maksym@test.com',
+  currentAddress: 'м. Київ, вул. Тестова, 1',
+  permanentAddress: 'м. Львів, вул. Друга, 2',
+};
---
+++ tests/laba1/lab1selectors.ts
@@ -0,0 +1,8 @@
+export const selectors = {
+  userNameInput: '#userName',
+  userEmailInput: '#userEmail',
+  currentAddressInput: '#currentAddress',
+  permanentAddressInput: '#permanentAddress',
+  submitButton: '#submit',
+  outputBox: '#output',
+};
